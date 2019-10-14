763675f013071b4ae7c30d4af6d16fefb9b18542
Anyi_11 <parkmei361@gmail.com>
2019-10-14 11:05:49 -0500

    Agrege acces

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php 
+$url = $_GET['url'];
+if ($url == 'login '){
+	include('templates/'.$url'.'.php')
+}
+
+ ?>
+
---
+++ login.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=divice-width, initial-scale-1,0">
+    <title>Deportes</title>
+    <link rel="stylesheet" href="estilos.css">
+  </head>
+  <body>
+    <nav>
+    <form action="" method="post">
+      <input type="text" name="user" placeholder="usuario">
+      <input type="password" name="user" placeholder="contraseña">
+    </form>
+  </nav>
+</body>
+</html>
